24428fbaea5bd2ed2e46a1cb89b3efb6e38177e9
goramate <matgora@gmail.com>
2016-11-08 16:29:04 +0100

    Wersja startowa

---
+++ controllers/homeControllers.js
@@ -0,0 +1,62 @@
+﻿angular.module("homeApp")
+.constant("authUrl", "http://127.0.0.1/silex/web/api_login_check")
+.constant("ordersUrl", "http://127.0.0.1/crm2/api/")
+.controller("authCtrl", function($scope, $http, $location, authUrl) {
+
+	
+    $scope.authenticate = function (user, pass) {
+        $http.post(authUrl,
+				"_username="+user+ "&_password=" +pass,
+				{ headers: {'Content-Type': 'application/x-www-form-urlencoded'}  }
+				).then( function(data) {
+						window.alert("Zalogowałeś się " + data.data);
+						$scope.authenticationError = null;
+						$scope.response = data.data;
+						$location.path("/main");
+					},
+				function(error) 
+        {
+			window.alert("Błąd:\n" + error.data );
+            $scope.authenticationError = error;
+			
+        });
+		
+    }
+	
+	$scope.test = function () {
+        $http.get("http://127.0.0.1/silex/web/api_wer",
+				"",
+				{  }
+				).then( function(data) {	
+						$location.path("/login");
+						$scope.authenticationError = null;
+						$scope.response = data.data;
+					},
+				function(error) 
+        {
+			window.alert("Błąd:\n" + error.data );
+            $scope.authenticationError = error;
+			
+        });
+		
+    }
+	
+	
+	$scope.logout = function () {
+        $http.get("http://127.0.0.1/silex/web/api_logout",
+				"",
+				{  }
+				).then( function(data) {
+						window.alert("Wylogowałeś się\n" + data);
+						$location.path("/login");
+					},
+				function(error) 
+        {
+			window.alert("Błąd:\n" + error );
+            $scope.authenticationError = error;
+			
+        });
+		
+    }
+	
+});
---
+++ controllers/mainCtrl.js
@@ -0,0 +1,159 @@
+﻿angular.module("homeApp")
+.constant("authUrl", "http://127.0.0.1/silex/web/")
+.controller("mainCtrl", function($scope, $http, $location, authUrl) {
+
+	
+	/*domyślny język z którego tłumaczymy*/
+	$scope.lang_version = 'polish';	
+	
+   /*domyślne ukrycie formularza dodawawnia słów*/
+   $scope.show_word_forms = false;
+   
+   /*licznik zaktualizowanych rekordów*/
+   $scope.updated_words =0;
+   $scope.updated_words = 0;
+   
+	/*funkcja testowa*/
+	$scope.test = function () {
+        $http.get("http://127.0.0.1/silex/web/api_wer",
+				"",
+				{  }
+				).then( function(data) {						
+						$scope.authenticationError = null;
+						$scope.response = data.data;
+					},
+				function(error) 
+        {
+			window.alert("Błąd:\n" + error.data );
+            $scope.authenticationError = error;
+			
+        });
+		
+    }
+	
+	
+	
+	/*pobranie listy słówek*/
+	$scope.get_word_list = function () {
+        $http.get("http://127.0.0.1/silex/web/api_word_list",
+				"",
+				{  }
+				).then( function(response) {						
+						$scope.authenticationError = null;
+						$scope.response = response.data;
+						$scope.words = JSON.parse(''+ response.data.data);
+						
+					},
+				function(error) 
+        {
+			window.alert("Błąd:\n" + error.data );
+            $scope.authenticationError = error;
+			
+        });
+		
+    }
+	
+	
+	
+	$scope.get_word_list();
+	/*aktualizacja danych */
+	$scope.update_word = function (item,interval) {
+			item.hide = true;
+			item.updated = true;
+			item.balance =  parseInt(item.balance) + interval;		
+			item.reply_number =  parseInt(item.reply_number) +1;			
+			var d = new Date();
+			item.last_reply =ISODateString(d);
+			$scope.updated_words++;	
+			$scope.total_updated_words++;
+			if($scope.updated_words >=10 ){			
+				synchronize_words($scope.words);
+				$scope.updated_words = 0;
+			}
+			if($scope.total_updated_words == 30){
+				$scope.total_updated_words =0;
+				//$scope.get_word_list();
+			}
+			
+	}
+	
+	
+	$scope.synchronize_words = function (items) {
+			synchronize_words(items);
+		
+	}
+	
+	/*synchronizacja danych*/
+	function synchronize_words(items) {
+		if( typeof items === 'undefined' || items === null ){
+			//TODO co jak zmienna jest pusta
+			console.log("Próba synchronizacji pustej tabeli");
+		}
+		else {
+		 $http.post("http://127.0.0.1/silex/web/api_update_words",
+				items,
+				{  }
+				).then( function(response) {						
+					$scope.response = response.data;	
+						
+					},
+				function(error) 
+			{
+				window.alert("Błąd:\n" + error.data );          
+			
+        });
+		
+		}
+	}
+	
+	
+	
+	
+	
+	
+	/*ustawienie języka*/
+	$scope.set_language = function (lang_version) {
+       $scope.lang_version = lang_version;
+		
+    }
+	
+	/*ustawienie widoczności szczegółów*/
+	$scope.show_details = function () {
+		if($scope.details){
+				 $scope.details = false;
+		}
+		else {
+				$scope.details = true;
+		}
+		
+    }
+	
+	
+	/**/
+	$scope.show_words_form = function () {
+		if($scope.show_word_forms){
+				 $scope.show_word_forms = false;
+		}
+		else {
+				$scope.show_word_forms = true;
+		}
+		
+    }
+	
+	
+	
+	/*konwersja daty do postaci poprawnej*/
+	function ISODateString(d){
+    function pad(n){return n<10 ? '0'+n : n}
+    return d.getUTCFullYear()+'-'
+    + pad(d.getUTCMonth()+1)+'-'
+    + pad(d.getUTCDate())+' '
+    + pad(d.getUTCHours())+':'
+    + pad(d.getUTCMinutes())+':'
+    + pad(d.getUTCSeconds())+''
+}
+	
+	
+	
+	
+});
